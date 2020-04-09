d0ec911844937030d93283d60182727573b728b7
mahmudur320 <mahmudurlimon41@gmail.com>
2020-04-09 13:05:41 +0600

    first cuda file

---
+++ cudanew/cudanew/mian.cu
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cuda.h>
+#include<cuda_runtime.h>
+
+using namespace std;
+
+__global__ void AddInstsCUDA(int* a, int *b)
+{
+	a[0] += b[0];
+}
+
+int main()
+{
+	int a = 5, b = 9;
+	int* d_a, * d_b;
+
+	cudaMalloc(&d_a, sizeof(int));
+	cudaMalloc(&d_b, sizeof(int));
+
+	cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+	AddInstsCUDA << <1, 1 >> > (d_a, d_b);
+
+	cudaMemcpy(&a, d_a, sizeof(int), cudaMemcpyDeviceToHost);
+
+	cout << "The answer is " << a << endl;
+
+	CudaFree(d_a);
+	CudaFree(d_b);
+
+	return 0;
+}
