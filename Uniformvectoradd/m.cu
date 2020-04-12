88744e3c95c0e2df5a4977502ea53132d8af76e5
mahmudur320 <mahmudurlimon41@gmail.com>
2020-04-12 22:36:47 +0600

    update file

---
+++ Uniformvectoradd/m.cu
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <cassert>
+#include <iostream>
+#include<cuda.h>
+#include<cuda_runtime.h>
+//#include<device_launch_parameter.h>
+
+using namespace std;
+
+using std::cout;
+
+__global__ void vectorAdd(int* a, int* b, int* c, int N) {
+    // Calculate global thread thread ID
+    int tid = (blockDim.x * blockIdx.x) + threadIdx.x;
+    //int tid = 10;
+    // Boundary check
+    if (tid < N) {
+        c[tid] = a[tid] + b[tid];
+    }
+}
+
+int main() {
+    // Array size of 2^16 (65536 elements)
+    const int N = 1 << 16;
+    size_t bytes = N * sizeof(int);
+
+    // Declare unified memory pointers
+    int* a, * b, * c;
+
+    // Allocation memory for these pointers
+    cudaMallocManaged(&a, bytes);
+    cudaMallocManaged(&b, bytes);
+    cudaMallocManaged(&c, bytes);
+
+    // Initialize vectors
+    for (int i = 0; i < N; i++) {
+        a[i] = rand() % 100;
+        b[i] = rand() % 100;
+    }
+
+    // Threads per CTA (1024 threads per CTA)
+    int BLOCK_SIZE = 1 << 10;
+
+    // CTAs per Grid
+    int GRID_SIZE = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    // Call CUDA kernel
+    vectorAdd << < GRID_SIZE, BLOCK_SIZE >> > (a, b, c, N);
+
+    // Wait for all previous operations before using values
+    // We need this because we don't get the implicit synchronization of
+    // cudaMemcpy like in the original example
+    cudaDeviceSynchronize();
+
+    // Verify the result on the CPU
+    for (int i = 0; i < N; i++) {
+        assert(c[i] == a[i] + b[i]);
+    }
+
+    // Free unified memory (same as memory allocated with cudaMalloc)
+    cudaFree(a);
+    cudaFree(b);
+    cudaFree(c);
+
+    std::cout << "COMPLETED SUCCESSFULLY!\n";
+
+    return 0;
+}
