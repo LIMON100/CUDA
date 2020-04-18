06c0a6666e22c76ce7bfc18408268984c8f1a56b
mahmudur320 <mahmudurlimon41@gmail.com>
2020-04-18 12:34:55 +0600

    update the main file

--- Matrxmultiplication/matmul.cu
+++ Matrxmultiplication/matmul.cu
@@ -46,5 +46,46 @@ void verify_result(vector<int>& a, vector<int>& b, vector<int>& c, int N) {
 
 int main()
 {
+    
+    int N = 1 << 10;
+
+    size_t bytes = N * N * sizeof(int);
+
+    vector<int> h_a(N * N);
+    vector<int> h_b(N * N);
+    vector<int> h_c(N * N);
+
+    generate(h_a.begin(), h_a.end(), []() { return rand() % 100; });
+    generate(h_b.begin(), h_b.end(), []() { return rand() % 100; });
+
+    int *d_a, * d_b, * d_c;
+
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
+
+    cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice);
+
+
+    int THREADS = 32;
+
+    int BLOCKS = N / THREADS;
+
+    dim3 threads(THREADS, THREADS);
+    dim3 blocks(BLOCKS, BLOCKS);
+
+    MatrixMul << <blocks, threads >> > (d_a, d_b, d_c, N);
+
+    cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyHostToDevice);
+
+    verify_result(h_a, h_b, h_c , N);
+
+    cout << "Completed Succesfully\n";
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
 	return 0;
 }
