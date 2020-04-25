484219edb46fbbdc031b43cea184bf1ebe499f8e
mahmudur320 <mahmudurlimon41@gmail.com>
2020-04-26 01:24:32 +0600

    multiplication on cache

---
+++ Cache Tiled Multiplicaiton/multiplication.cu
@@ -0,0 +1,123 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <vector>
+#include<cuda.h>
+#include<cuda_runtime.h>
+#include<device_launch_parameters.h>
+#include <cuda_runtime_api.h>
+#include <device_functions.h>
+#include<stdio.h>
+
+using std::cout;
+using std::generate;
+using std::vector;
+
+//pull out matrix and shared memory tile size
+
+const int N = 1 << 10;
+const int SHMEM_SIZE = 1 << 10;
+
+
+__global__ void matrixTiledMultiplication(const int* a, const int* b, int* c) {
+
+		int row = blockIdx.y * blockDim.y + threadIdx.y;
+		int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+		// Statically allocated shared memory
+		__shared__ int s_a[SHMEM_SIZE];
+		__shared__ int s_b[SHMEM_SIZE];
+
+		int tmp = 0;
+
+		//Load in elements for this tiles
+		for (int i = 0; i < N; i += blockDim.x) {
+
+			s_a[threadIdx.y * blockDim.x + threadIdx.x] = a[row * N + i + threadIdx.x];
+			s_b[threadIdx.y * blockDim.x + threadIdx.x] = b[i * N + threadIdx.y * N + col];
+
+			__syncthreads();
+
+
+			for (int j = 0; j < blockDim.x; j++) {
+
+				tmp += s_a[threadIdx.y * blockDim.x + j] * s_b[j * blockDim.x + threadIdx.x];
+
+			}
+
+			// Wait for all threads to finish using current tiles before loading in new
+			// ones
+			__syncthreads();
+
+			c[row * N + col] = tmp;
+
+		}
+
+
+}
+
+
+void verify_result(vector<int>&a , vector<int>&b , vector<int>&c) {
+
+	for (int i = 0; i < N; i++) {
+
+		for (int j = 0; j < N; j++) {
+
+			int tmp = 0;
+			for (int k = 0; k < N; k++) {
+
+				tmp += a[i * N + k] * b[k * N + j];
+			}
+
+			assert(tmp == c[i * N + j]);
+		}
+	}
+
+}
+
+
+int main()
+{
+	size_t bytes = N * N * sizeof(int);
+
+	vector<int>h_a(N * N);
+	vector<int>h_b(N * N);
+	vector<int>h_c(N * N);
+
+	generate(h_a.begin(), h_a.end(), []() { return  rand() % 100; });
+	generate(h_b.begin(), h_b.end(), []() { return  rand() % 100; });
+
+	int* d_a, * d_b, * d_c;
+
+	cudaMalloc(&d_a, bytes);
+	cudaMalloc(&d_b, bytes);
+	cudaMalloc(&d_c, bytes);
+
+	cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice);
+
+	int THREADS = 32;
+	int BLOCKS = N / THREADS;
+
+
+	dim3 threads(THREADS, THREADS);
+	dim3 blocks(BLOCKS, BLOCKS);
+
+
+	matrixTiledMultiplication << < blocks , threads >> > (d_a, d_b, d_c);
+
+	cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
+
+	verify_result(h_a, h_b, h_c);
+
+	cout << "COMPLETED SUCCESSFULLY\n";
+
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_c);
+
+
+	return 0;
+}
